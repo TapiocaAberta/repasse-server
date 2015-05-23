5a22ddd655df6c9a9321f83cb56d4737dde73beb
jesuino <william.a.siqueira@gmail.com>
2015-05-22 23:24:41 -0300

    Melhorando processo de carga e adicionando página de administração básica

--- transfgov-server/src/main/java/org/jugvale/transfgov/resource/TransferenciaResource.java
+++ transfgov-server/src/main/java/org/jugvale/transfgov/resource/TransferenciaResource.java
@@ -2,10 +2,12 @@ package org.jugvale.transfgov.resource;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.jugvale.transfgov.model.transferencia.Transferencia;
 import org.jugvale.transfgov.resource.config.jsonview.MunicipioConcisoView;
@@ -13,8 +15,10 @@ import org.jugvale.transfgov.resource.config.jsonview.MunicipioConcisoView;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @Path("transferencia")
-@Produces("application/json; charset=utf8")
 public interface TransferenciaResource {
+
+	static final String MSG_NAO_HA_TRANSFERENCIA = "Não há transferências carregadas para %d/%d";
+	static final String MSG_TRANSFERENCIA_APAGADAS = "Transferências carregadas para %d/%d apagadas";
 
 	/**
 	 * 
@@ -28,6 +32,15 @@ public interface TransferenciaResource {
 	@GET
 	@Path("{ano}/{mes}/municipio/{municipioId}")
 	@JsonView(MunicipioConcisoView.class)
-	public List<Transferencia> porAnoMesMunicipio(@PathParam("ano") int ano,@PathParam("mes") int mes, @PathParam("municipioId") long municipioId);
+	@Produces("application/json; charset=utf8")
+	public List<Transferencia> porAnoMesMunicipio(@PathParam("ano") int ano,
+			@PathParam("mes") int mes,
+			@PathParam("municipioId") long municipioId);
+
+	@DELETE
+	@Path("{ano}/{mes}")
+	@Produces("text/plan; charset=utf8")
+	public Response apagaPorMesAno(@PathParam("ano") int ano,
+			@PathParam("mes") int mes);
 
 }
--- transfgov-server/src/main/java/org/jugvale/transfgov/resource/impl/TransferenciaResourceImpl.java
+++ transfgov-server/src/main/java/org/jugvale/transfgov/resource/impl/TransferenciaResourceImpl.java
@@ -3,6 +3,8 @@ package org.jugvale.transfgov.resource.impl;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.jugvale.transfgov.model.base.Municipio;
 import org.jugvale.transfgov.model.transferencia.Transferencia;
@@ -11,7 +13,7 @@ import org.jugvale.transfgov.service.impl.MunicipioService;
 import org.jugvale.transfgov.service.impl.TransferenciaService;
 import org.jugvale.transfgov.utils.JaxrsUtils;
 
-public class TransferenciaResourceImpl implements TransferenciaResource{
+public class TransferenciaResourceImpl implements TransferenciaResource {
 	
 	@Inject
 	TransferenciaService transferenciaService;
@@ -32,5 +34,16 @@ public class TransferenciaResourceImpl implements TransferenciaResource{
 		Municipio municipio = JaxrsUtils.lanca404SeNulo(municipioService.buscarPorId(municipioId), Municipio.class);		
 		return transferenciaService.buscarPorAnoMesMunicipio(ano, mes, municipio);
 	}
+	
+	public Response apagaPorMesAno(int ano, int mes){
+		JaxrsUtils.lanca404SeFalso(transferenciaService.temTranferencia(ano, mes), String.format(MSG_NAO_HA_TRANSFERENCIA, mes, ano));
+		try {
+			transferenciaService.apagaTransferencias(ano, mes);
+			return Response.ok().entity(String.format(MSG_TRANSFERENCIA_APAGADAS, mes, ano)).build();
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build();
+		}
+		
+	}	
 
 }

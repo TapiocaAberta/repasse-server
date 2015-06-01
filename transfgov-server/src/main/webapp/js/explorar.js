0fcc6fe484ba7f1da193bc6ddd6bb2fde2f39def
jesuino <william.a.siqueira@gmail.com>
2015-06-01 00:20:36 -0300

    ordenando resultados da view. closes #36

--- transfgov-server/src/main/webapp/js/explorar.js
+++ transfgov-server/src/main/webapp/js/explorar.js
@@ -5,79 +5,91 @@ function inicializa($scope, $http) {
 		e.preventDefault();
 		$(this).tab('show');
 	});
-	$http.get("./rest/agregacao").success(function(agregacoes) {
-		$scope.agregacoes =  $.grep(agregacoes, function(agregacao) {
-			return agregacao != 'MUNICIPIO' && agregacao != 'MES' && agregacao != 'ANO';
-		});
-		 $scope.agregacaoSelecionada = $scope.agregacoes[0];
-	});	
+	$http.get("./rest/agregacao").success(
+			function(agregacoes) {
+				$scope.agregacoes = $.grep(agregacoes, function(agregacao) {
+					return agregacao != 'MUNICIPIO' && agregacao != 'MES'
+							&& agregacao != 'ANO';
+				});
+				$scope.agregacaoSelecionada = $scope.agregacoes[0];
+			});
 
 }
 
 function criaGraficoAnoArea(agregacoesAno) {
-	var categorias = new Array(); 
+	var categorias = new Array();
 	var seriesMap = {};
 	var ano;
 	var series = new Array();
 	agregacoesAno.forEach(function(agregacaoAno) {
 		ano = agregacaoAno.ano;
 		categorias.push(agregacaoAno.mes);
-		for(a in agregacaoAno.dadosAgregados){
-			if(!seriesMap[a]) seriesMap[a] = new Array();
+		for (a in agregacaoAno.dadosAgregados) {
+			if (!seriesMap[a])
+				seriesMap[a] = new Array();
 			seriesMap[a].push(agregacaoAno.dadosAgregados[a]);
 		}
-	});	
-	for(s in seriesMap) {
+	});
+	for (s in seriesMap) {
 		series.push({
-			name: s,
-			data: seriesMap[s]
+			name : s,
+			data : seriesMap[s]
 		});
 	}
-	  $('#divGraficoAreaPorAno').highcharts({
-	        title: {
-	            text: 'Tranferências no ano ' + ano,
-	            x: -20 //center
-	        },
-	        subtitle: {
-	            text: 'Agregadas todas as transferências no ano de ' + ano,
-	            x: -20
-	        },
-	        xAxis: {
-	        	title: {
-	                text: 'Mês'
-	            },
-	            categories: categorias
-	        },	        
-	        tooltip: {
-	        	valuePrefix: "R$ "	        	
-	        },
-	        yAxis: {
-	            title: {
-	                text: 'Valor'
-	            },
-	            plotLines: [{
-	                value: 0,
-	                width: 1,
-	                color: '#808080'
-	            }]
-	        },
-	        legend: {
-	        	itemWidth: 200,
-	            layout: 'vertical',
-	            align: 'right',
-	            verticalAlign: 'middle',
-	            borderWidth: 0
-	        },
-	        series: series
-	    });	
+	$('#divGraficoAreaPorAno').highcharts({
+		title : {
+			text : 'Tranferências no ano ' + ano,
+			x : -20
+		// center
+		},
+		subtitle : {
+			text : 'Agregadas todas as transferências no ano de ' + ano,
+			x : -20
+		},
+		xAxis : {
+			title : {
+				text : 'Mês'
+			},
+			categories : categorias
+		},
+		tooltip : {
+			valuePrefix : "R$ "
+		},
+		yAxis : {
+			title : {
+				text : 'Valor'
+			},
+			plotLines : [ {
+				value : 0,
+				width : 1,
+				color : '#808080'
+			} ]
+		},
+		legend : {
+			itemWidth : 200,
+			layout : 'vertical',
+			align : 'right',
+			verticalAlign : 'middle',
+			borderWidth : 0
+		},
+		series : series
+	});
 }
 
 appExplorar.controller('ExplorarController', function($scope, $http) {
-	inicializa($scope, $http);	
+	inicializa($scope, $http);
 	$http.get("./rest/ano").success(function(anos) {
 		$scope.anos = anos;
 	});
 	$http.get("./rest/estado").success(function(estados) {
+		estados.sort(function(a, b) {
+			if (a.sigla < b.sigla)
+				return -1;
+			if (a.sigla > b.sigla)
+				return 1;
+			return 0;
+
+		});
 		$scope.estados = estados;
 	});
 	$scope.carregaMunicipios = function() {
@@ -91,91 +103,95 @@ appExplorar.controller('ExplorarController', function($scope, $http) {
 		$scope.carregaAgregacaoAno();
 		$scope.municipioBusca = $scope.municipioSelecionado;
 	}
-	
-	$scope.listenerAgregacao = function(){
+
+	$scope.listenerAgregacao = function() {
 		$scope.carregaAgregacaoAno();
 		$scope.carregaDadosMes();
 	}
-	
-	$scope.carregaAgregacaoAno = function(){
+
+	$scope.carregaAgregacaoAno = function() {
 		var ano = $scope.anoSelecionado.ano;
 		var id = $scope.municipioSelecionado.id;
 		var agreg = $scope.agregacaoSelecionada;
 		$scope.anoBusca = ano;
 		$scope.municipioBusca = $scope.municipioSelecionado;
-		var uriTransfAno = "rest/agregacao/ANO/" + ano + "/" + agreg + "/municipio/" + id;
+		var uriTransfAno = "rest/agregacao/ANO/" + ano + "/" + agreg
+				+ "/municipio/" + id;
 		$http.get(uriTransfAno).success(criaGraficoAnoArea);
 	}
-	
-	$scope.carregaDadosMes = function() {	
-		if(!$scope.mesSelecionado) {
+
+	$scope.carregaDadosMes = function() {
+		if (!$scope.mesSelecionado) {
 			return;
 		}
 		var ano = $scope.anoSelecionado.ano;
 		var mes = $scope.mesSelecionado;
 		var id = $scope.municipioSelecionado.id;
-		var uriTransfMes = "rest/transferencia/" + ano + "/" + mes + "/municipio/"
+		var uriTransfMes = "rest/transferencia/" + ano + "/" + mes
+				+ "/municipio/" + id;
+		var uriTransfAno = "rest/agregacao/ANO/" + ano + "/AREA/municipio/"
 				+ id;
-		var uriTransfAno = "rest/agregacao/ANO/" + ano + "/AREA/municipio/" + id;
 		$http.get(uriTransfMes).success(function(transferencias) {
 			$scope.transferenciasMes = transferencias;
 			$scope.carregaGraficosAgregacao();
 		});
 	}
-	
-	$scope.carregaGraficosAgregacao = function() {		
+
+	$scope.carregaGraficosAgregacao = function() {
 		var a = $scope.agregacaoSelecionada;
 		var ano = $scope.anoSelecionado.ano;
 		var mes = $scope.mesSelecionado;
 		var id = $scope.municipioSelecionado.id;
-		if(!a) return;
+		if (!a)
+			return;
 		$scope.gerandoGraficoAgregacao = true;
-		var uriAgregacao =  a + "/"+ ano+ "/" + mes + "/municipio/"+ id;
-		$http.get("./rest/agregacao/" + uriAgregacao).success(function(agregacao) {
-			$scope.dadosAgregados= agregacao.dadosAgregados;
-			var dadosGrafico = new Array();
-			for(i in agregacao.dadosAgregados) {
-				dadosGrafico.push({
-                    name: i,
-                    y: agregacao.dadosAgregados[i]
-                });
-			}
-			$('#containerPizzaAgregacao').highcharts({
-		        chart: {
-		            plotBackgroundColor: null,
-		            plotBorderWidth: null,
-		            plotShadow: false
-		        },
-		        title: {
-		            text: 'Dados agregados por ' + a
-		        },
-		        tooltip: {
-		            pointFormat: '<b>{point.percentage:.1f}%</b>'
-		        },
-		        legend: {
-		        	itemWidth: 250,
-		            layout: 'vertical',
-		            align: 'right',
-		            verticalAlign: 'middle',
-		            borderWidth: 0
-		        },
-		        plotOptions: {
-		            pie: {
-		                allowPointSelect: true,
-		                cursor: 'pointer',
-		                dataLabels: {
-		                    enabled: false,
-		                },
-	                    showInLegend: true
-		            }
-		        },
-		        series: [{
-		            type: 'pie',
-		            name: 'Dados por ' + a,
-		            data: dadosGrafico
-		        }]
-		    });
-		});
+		var uriAgregacao = a + "/" + ano + "/" + mes + "/municipio/" + id;
+		$http.get("./rest/agregacao/" + uriAgregacao).success(
+				function(agregacao) {
+					$scope.dadosAgregados = agregacao.dadosAgregados;
+					var dadosGrafico = new Array();
+					for (i in agregacao.dadosAgregados) {
+						dadosGrafico.push({
+							name : i,
+							y : agregacao.dadosAgregados[i]
+						});
+					}
+					$('#containerPizzaAgregacao').highcharts({
+						chart : {
+							plotBackgroundColor : null,
+							plotBorderWidth : null,
+							plotShadow : false
+						},
+						title : {
+							text : 'Dados agregados por ' + a
+						},
+						tooltip : {
+							pointFormat : '<b>{point.percentage:.1f}%</b>'
+						},
+						legend : {
+							itemWidth : 250,
+							layout : 'vertical',
+							align : 'right',
+							verticalAlign : 'middle',
+							borderWidth : 0
+						},
+						plotOptions : {
+							pie : {
+								allowPointSelect : true,
+								cursor : 'pointer',
+								dataLabels : {
+									enabled : false,
+								},
+								showInLegend : true
+							}
+						},
+						series : [ {
+							type : 'pie',
+							name : 'Dados por ' + a,
+							data : dadosGrafico
+						} ]
+					});
+				});
 		$scope.gerandoGraficoAgregacao = false;
 	}
 

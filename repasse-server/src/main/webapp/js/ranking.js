c9cdcc99b2bc1f3428483691302de62e3fd6415b
jesuino <william.a.siqueira@gmail.com>
2015-08-09 04:41:32 -0300

    Esqueci do JS e subir o persistence zoado

---
+++ repasse-server/src/main/webapp/js/ranking.js
@@ -0,0 +1,72 @@
+angular.module('RepasseApp', []).factory('repasseService', function($http) {
+	return new RepasseService($http)
+}).controller('RankingController', function($scope, repasseService) {
+	Highcharts.setOptions({
+		lang : {
+			decimalPoint : ',',
+			thousandsSep : '.'
+		}
+	}); /*
+		 * REMOVIDO TEMPORARIAMENTE repasseService.anos(function(d) {
+		 * $scope.anos = d; });
+		 */
+	$scope.anos = ANOS;
+	
+	$scope.carregaRanking = function() {
+		var ano  = $scope.anoSelecionado;
+		$scope.carregando = true;
+		repasseService.rankingPorAno(ano, function(ranking){
+			$scope.carregando = false;
+			montarGraficoRanking(ranking);
+		});
+	}
+});
+
+function montarGraficoRanking(ranking) {
+	var ano = ranking.ano;
+	var titulo = ranking.nome;
+	var categorias = [];
+	var dados = [];
+	for(i in ranking.resultados) {
+		var res = ranking.resultados[i];
+		categorias.push(res.nomeCidade);
+		dados.push(
+			 res.valorPerCapita
+		);
+	}
+	$("#graficoRanking").highcharts({
+			 chart: {
+		            type: 'column'
+		        },
+		        title: {
+		            text: titulo
+		        },			       
+		        subtitle: {
+		            text: 'Municípios que mais receberam repasse do governo federal <i>per capita</i>'
+		        },
+		        xAxis: {
+		            categories: categorias,
+		            crosshair: true
+		        },
+		        yAxis: {
+		            title: {
+		                text: 'Valor <i>per capita</i>'
+		            },
+			        labels: {
+			            enabled: false
+			        }
+		        },
+		        tooltip: {
+		            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+		            pointFormat: '<tr><td style="color:{series.color};padding:0">Valor <i>per capita</i>: </td>' +
+		                '<td style="padding:0"><b>R$ {point.y:,.3f} </b></td></tr>',
+		            footerFormat: '</table>',
+		            shared: true,
+		            useHTML: true
+		        },
+		        series: [{
+		        	data: dados
+		        }]
+		});
+	
+}

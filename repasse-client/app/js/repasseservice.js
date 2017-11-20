fa2db7237ed877b4164dff4a2757fb1235597755
William Siqueira <william.fatecsjc@gmail.com>
2017-11-20 21:08:07 -0200

    closes #205

--- repasse-client/app/js/repasseservice.js
+++ repasse-client/app/js/repasseservice.js
@@ -423,3 +423,14 @@ function createTableFilter(table, field) {
         }).show();
       });
 }
+
+function shareCurrentPageOnFacebook() {
+    window.open("https://www.facebook.com/sharer/sharer.php?u="+escape(window.location.href)+"&t="+document.title, '', 
+        'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+    return false;
+}
+
+function tweetCurrentPage() { 
+    window.open("https://twitter.com/share?url="+escape(window.location.href)+"&text="+document.title, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+    return false; 
+}
